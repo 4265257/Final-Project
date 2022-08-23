0ae563a269981ff5966d7e7d64d82aadc5506114
“Yara <yarabrek123@gmail.com>
2022-08-22 22:17:40 -0700

    YaraBrek-workingOnSigninPage

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { Auth0Provider } from "@auth0/auth0-react"
 //import './index.css';
 //import reportWebVitals from './reportWebVitals';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN
+const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain={domain}
+    clientId={clientID}
+    redirectUri={window.location.origin}
+    >
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
